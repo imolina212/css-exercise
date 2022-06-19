15260fabb62a9b1d1265097ac30afee5e0604382
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-06-19 15:16:53 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h4>VIEWS FROM OUR COLUMNISTS</h4>
+      <div>
+        <h3>YVONNE ABRAHAM</h3>
+        <p>Nativity School of Worcester now prohibited from
+calling itself a Catholic school</p>
+      </div>
+      <div>
+        <h3>JENEÉ OSTERHELDT</h3>
+        <p>Our children don't feel safe. Gun control, not
+more guns, is the way forward.</p>
+      </div>
+      <div>
+        <h3>ADRIAN WALKER</h3>
+        <p>How did Patrick Rose keep his badge? And who
+decided?</p>
+      </div>
+      <div>
+        <h3>SHIRLEY LEUNG</h3>
+        <p>A new park is unfolding in the Seaport. Is it what
+the neighborhood needs?</p>
+      </div>
+      <div>
+        <h3>THOMAS FARRAGHER</h3>
+        <p>A half century of vigilance in the big poolside chair</p>
+      </div>
+      <div>
+        <h3>KEVIN CULLEN</h3>
+        <p>Gun violence visits a small, idyllic Vermont town</p>
+      </div>
+      <div>
+        <h3>DAN MCGOWAN</h3>
+        <p>What it was like to dine with Buddy Cianci at the
+Old Canteen in Providence</p>
+      </div>
     </div>
   );
 }
